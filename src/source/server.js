a9a015b13878d9b17118c8a03e9794416d384669
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-02-22 23:48:15 -0600

    Add initial login page (no integration)

--- src/source/server.js
+++ src/source/server.js
@@ -5,23 +5,26 @@ var io = require("socket.io")(server);
 let registry = [];
 let port = process.env.PORT || 8080;
 server.listen(port);
-
+let loadData = {}
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/login.html");
 });
 
-app.get("/index/", (req, res) => {
-  res.sendFile(__dirname + "/index.js");
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  loadData.usr = req.query.usr
+  loadData.chat = req.query.chat
+  loadData.color = require('randomcolor')()
 });
 
 io.on("connection", socket => {
-  socket.emit("load"); // Onload functions
-  socket.emit("assign color", require('randomcolor')()); // Assign user color
+  socket.emit("load", loadData); // Onload functions
+  loadData = {}
 
   //  socket.join('admin') // join default chat
   //socket.emit("load", { chat: modules.loadChat() }); // Load chat history on init
   socket.on("message", data => {
-    io.emit("message", data); // When recive message from client, broadcast that message to all clients (including sender)
+    socket.emit("message", data); // When recive message from client, broadcast that message to all clients (including sender)
   });
   socket.on('new id', id => {
     io.emit('scroll', id)
