b5916aa6ac57570ebdb4de0046e872b293e8f69b
Amrit <33424649+Spaceface16518@users.noreply.github.com>
2018-02-23 23:44:06 -0600

    Responsive (#6)
    * Add meta tags
    
    * Add autoscroll on input focus
    
    * Disable user zoom
    
    * Inital commit on ios keyboard support
    
    * Patch form position overlay bug
    
    * Patch list underlay color bug
    
    * Username and Chat Selection (#13)
    
    * Add initial login page (no integration)
    
    * Update package.json to match app version
    
    * Implement join/login functionality
    
    * Add favicon
    
    * Change index page title
    
    
    **IOS still does not work**
    
    Not a priority though. Get essentials first, then other stuff.

--- src/source/server.js
+++ src/source/server.js
@@ -5,25 +5,38 @@ var io = require("socket.io")(server);
 let registry = [];
 let port = process.env.PORT || 8080;
 server.listen(port);
-
+let loadData = {};
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/login.html");
 });
 
-app.get("/index/", (req, res) => {
-  res.sendFile(__dirname + "/index.js");
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  loadData.usr = req.query.usr;
+  loadData.chat = req.query.chat;
+  loadData.color = require("randomcolor")();
+});
+app.get("/favicon", (req, res) => {
+  res.sendFile(__dirname + "/custom.png");
 });
 
 io.on("connection", socket => {
-  socket.emit("load"); // Onload functions
-  socket.emit("assign color", require('randomcolor')()); // Assign user color
-
-  //  socket.join('admin') // join default chat
-  //socket.emit("load", { chat: modules.loadChat() }); // Load chat history on init
-  socket.on("message", data => {
-    io.emit("message", data); // When recive message from client, broadcast that message to all clients (including sender)
+  socket.emit("load", loadData); // Onload functions
+  socket.join(loadData.chat);
+  loadData = {};
+  io.to("alien5").emit("message", {
+    id: Math.random()
+      .toString(36)
+      .slice(2),
+    msg: "Testing, testing; this thing on?",
+    usr: "alien",
+    chat: "alien5",
+    color: "#f49842",
+    time: new Date()
   });
-  socket.on('new id', id => {
-    io.emit('scroll', id)
-  })
+
+  // TODO: Load chat history on init, with mongoDB
+  socket.on("message", data => {
+    io.emit("message", data); // When recive message from client, broadcast that message
+  });
 });
