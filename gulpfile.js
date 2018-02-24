ede0c5aa4028f4d2228e7c87c6092b5ddfc46f54
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-02-24 13:34:59 -0600

    Upgrade UX

--- gulpfile.js
+++ gulpfile.js
@@ -13,5 +13,5 @@ gulp.task("default", () => {
   gulp.src("./src/source/server.js").pipe(gulp.dest("./lib"));
 
   log('Moving favicons');
-  gulp.src('./src/source/assets/pics/*').pipe(gulp.dest('./lib'))
+  gulp.src('./src/source/assets/pics/**/*').pipe(gulp.dest('./lib'))
 });
